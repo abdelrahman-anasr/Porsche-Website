b95ec124b1b4945655acde14094ac54c4cf10d13
abdelrahman-anasr <abdulrahman.nasr@student.giu-uni.de>
2024-03-14 17:16:02 +0200

    V0.41 Major Changes to Sidebar

--- Porsche-website/script.js
+++ Porsche-website/script.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("sidebar").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
   }
   
 
@@ -8,3 +8,13 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   } 
+
+  function opensecondNav() {
+    document.getElementById("secondSidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  function closeSecondNav() {
+    document.getElementById("secondSidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
